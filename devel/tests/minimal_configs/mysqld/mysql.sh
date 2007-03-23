99fdb78f36a8e9bb0c7fa58ce254323330f952f4
Sylvain Beucler <beuc@beuc.net>
2007-03-23 22:32:35 +0000

    autoconfiscated database creation + notes on how to test it

--- devel/tests/minimal_configs/mysqld/mysql.sh
+++ devel/tests/minimal_configs/mysqld/mysql.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export PATH=$PATH:/usr/local/sbin:/usr/sbin:/sbin
 opt="--no-defaults --pid-file=pid --socket=sock --skip-networking --datadir=`pwd`/db"
 mysql_install_db $opt
 mysqld $opt
