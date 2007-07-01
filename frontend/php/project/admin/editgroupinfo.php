1d1cb8fb9a1aa37a1412e48eb5b6699e0b04d8ed
Sylvain Beucler <beuc@beuc.net>
2007-07-01 14:57:43 +0200

    editgroupinfo.php cleanup

--- frontend/php/project/admin/editgroupinfo.php
+++ frontend/php/project/admin/editgroupinfo.php
@@ -1,21 +1,19 @@
 <?php
-# This file is part of the Savane project
-# <http://gna.org/projects/savane/>
+# Edit project name/description/maturity
+# Copyright 1999-2000 (c) The SourceForge Crew
+# Copyright 2000-2003 (c) Free Software Foundation
+#                         Mathieu Roy <yeupou--gnu.org>
+# Copyright 2004-2006 (c) Mathieu Roy <yeupou--gnu.org>
+# Copyright (C) 2007  Sylvain Beucler
 #
-# $Id$
-#
-#  Copyright 1999-2000 (c) The SourceForge Crew
-#  Copyright 2000-2003 (c) Free Software Foundation
-#                          Mathieu Roy <yeupou--gnu.org>
-#
-#  Copyright 2004-2006 (c) Mathieu Roy <yeupou--gnu.org>
-#
-# The Savane project is free software; you can redistribute it and/or
+# This file is part of Savane.
+# 
+# Savane is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
 #
-# The Savane project is distributed in the hope that it will be useful,
+# Savane is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
@@ -24,12 +22,8 @@
 # along with the Savane project; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-#input_is_safe();
-#mysql_is_safe();
-
 require_once('../../include/init.php');
 require_once('../../include/vars.php');
-register_globals_off();
 
 extract(sane_import('post', array('update',
   'form_group_name', 'form_shortdesc', 'form_longdesc', 'form_devel_status')));
