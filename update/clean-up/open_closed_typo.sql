91168d72bd9b4078b6a1e1bdb28539292080bb62
Sylvain Beucler <beuc@beuc.net>
2008-04-28 20:55:47 +0200

    s/death/dealt/g - caught by Reuben Thomas

---
+++ update/clean-up/open_closed_typo.sql
@@ -0,0 +1,9 @@
+## s/dealth/dealt/g
+
+UPDATE support_field SET description='Most basic status of the item: is the item considered as dealt with or not.' WHERE bug_field_id='101' LIMIT 1 ;
+
+UPDATE bugs_field SET description='Most basic status of the item: is the item considered as dealt with or not.' WHERE bug_field_id='101' LIMIT 1 ;
+
+UPDATE task_field SET description='Most basic status of the item: is the item considered as dealt with or not.' WHERE bug_field_id='101' LIMIT 1 ;
+
+UPDATE patch_field SET description='Most basic status of the item: is the item considered as dealt with or not.' WHERE bug_field_id='101' LIMIT 1 ;
