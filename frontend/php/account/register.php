3f6c384fbc3c84794b4b369b652a01de390b44ff
Sylvain Beucler <beuc@beuc.net>
2010-12-02 00:13:47 +0100

    typo

--- frontend/php/account/register.php
+++ frontend/php/account/register.php
@@ -287,7 +287,7 @@ else
   print '<p><span class="preinput">'._("Login Name:").'</span><br />&nbsp;&nbsp;';
   print form_input("text", "form_loginname", $form_loginname);
 
-  print '<p><span class="preinput">'._("Password / passphrase:")." "."(not too short, must contain multiple character classes: symbols, digits (0-9), upper and lower case letters)").'</span><br />&nbsp;&nbsp;';
+  print '<p><span class="preinput">'._("Password / passphrase:")." "._("(not too short, must contain multiple character classes: symbols, digits (0-9), upper and lower case letters)").'</span><br />&nbsp;&nbsp;';
   print form_input("password", "form_pw", $form_pw);
   print "</p>";
 
