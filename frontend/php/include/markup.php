8d6d0db8a8d0c52eb50b078a1801250a897bb593
Karl Berry <karl@gnu.org>
2016-07-06 22:27:29 +0000

    allow preceding [ for "rich text" markup, from sergey

--- frontend/php/include/markup.php
+++ frontend/php/include/markup.php
@@ -627,9 +627,10 @@ function _markup_inline($line)
   # we can expect web browsers to support)
   $protocols = "https?|ftp|sftp|file|afs|nfs";
 
-  # Prepare usual links: prefix every "www." with "http://"
-  # unless there is a // before
-  $line = preg_replace('/\b(www\.)/i', 'http://$1', $line);
+  # Prepare usual links: prefix "www." with "http://"
+  # if it is preceded by [ or whitespace or at the beginning of line.
+  # (don't want to prefix in cases like "//www.." or "ngwww...")
+  $line = preg_replace('/(^|\s|\[)(www\.)/i', '$1http://$2', $line); 
 
   # replace the @ sign with an HTML entity, if it is used within
   # an url (e.g. for pointers to mailing lists). This way, the
