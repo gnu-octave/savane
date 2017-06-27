ed6e46cbc1fcedada795b8e426f7df250d95f73f
Ineiev <ineiev@gnu.org>
2017-06-27 17:23:13 +0000

    add copyright and license notices

--- frontend/php/account/index.php
+++ frontend/php/account/index.php
@@ -1,5 +1,25 @@
 <?php
+# First valid login, after account confirmation
+#
+# Copyright (C) 2008 Sylvain Beucler
+#
+# This file is part of Savane.
+#
+# Savane is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# Savane is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 require_once('../include/init.php');
 require_once('../include/session.php');
 register_globals_off();
 session_redirect($GLOBALS['sys_home']."my/admin/");
+?>
