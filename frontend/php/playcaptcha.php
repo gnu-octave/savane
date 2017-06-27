2016523f6c12649c16f5b4254862cc12293582f3
Ineiev <ineiev@gnu.org>
2017-06-27 17:27:39 +0000

    reword in a more readable way

--- frontend/php/playcaptcha.php
+++ frontend/php/playcaptcha.php
@@ -1,10 +1,32 @@
 <?php
+# Captcha support.
+#
+# Copyright (C) 2012 Michael J. Flickinger
+# Copyright (C) 2017 Ineiev
+#
+# This file is part of Savane.
+#
+# Savane is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# Savane is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 require('include/init.php');
 include_once $GLOBALS['sys_securimagedir'] . '/securimage.php';
 
 $img = new Securimage();
-$img->audio_format = (isset($_GET['format']) && in_array(strtolower($_GET['format']), array('mp3', 'wav')) ? strtolower($_GET['format']) : 'mp3');
+$img->audio_format = 'mp3';
+if (isset($_GET['format']))
+  if (strtolower ($_GET['format']) == 'wav')
+    $img->audio_format = 'wav';
 $img->setAudioPath($GLOBALS['sys_securimagedir'] .  '/audio/');
 
 $img->outputAudioFile();
