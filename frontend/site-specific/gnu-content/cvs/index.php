5bdbfa61b92d98dd13e4018707c7a4424fe7507f
Ineiev <ineiev@gnu.org>
2017-07-29 13:58:17 +0000

    fix typos; update SSH fingerprints

--- frontend/site-specific/gnu-content/cvs/index.php
+++ frontend/site-specific/gnu-content/cvs/index.php
@@ -62,9 +62,9 @@ if ($project->CanUse("homepage") || $project->UsesForHomepage("cvs")) {
 }
 
 print '<p>'
-._("<em>Hint:</em>When you update your working copy from within the
+._("<em>Hint:</em> When you update your working copy from within the
 module's directory (with <em>cvs update</em>) you do not need the -d
-option anymore.  Simply use:").'</p>
+option anymore.  Simply use").'</p>
 
 <blockquote>
 <pre>
@@ -85,8 +85,9 @@ pserver method can only be used for anonymous access.').'</p>
 trees are:').'</p>
 
 <pre>
-RSA: 1024 80:5a:b0:0c:ec:93:66:29:49:7e:04:2b:fd:ba:2c:d5
-DSA: 1024 4d:c8:dc:9a:99:96:ae:cc:ce:d3:2b:b0:a3:a4:95:a5
+1024 80:5a:b0:0c:ec:93:66:29:49:7e:04:2b:fd:ba:2c:d5 (RSA)
+256 65:b8:1c:2f:82:7c:0e:39:e1:4a:63:f2:13:10:e8:9c (ECDSA)
+256 14:7b:c8:98:dd:06:08:97:8c:00:9d:d2:ae:85:c8:82 (ED25519)
 </pre>
 
 ';
--- frontend/site-specific/gnu-content/svn/index.php
+++ frontend/site-specific/gnu-content/svn/index.php
@@ -57,13 +57,14 @@ you're unsure, or browse the repository with ViewVC.").'</p>
 
 <p>
 '._('The SSHv2 public key fingerprints for the machine hosting the source
-trees are:').'
+trees are:').'</p>
 
 <pre>
-RSA: 1024 80:5a:b0:0c:ec:93:66:29:49:7e:04:2b:fd:ba:2c:d5
-DSA: 1024 4d:c8:dc:9a:99:96:ae:cc:ce:d3:2b:b0:a3:a4:95:a5
+1024 80:5a:b0:0c:ec:93:66:29:49:7e:04:2b:fd:ba:2c:d5 (RSA)
+256 65:b8:1c:2f:82:7c:0e:39:e1:4a:63:f2:13:10:e8:9c (ECDSA)
+256 14:7b:c8:98:dd:06:08:97:8c:00:9d:d2:ae:85:c8:82 (ED25519)
 </pre>
-</p>
+
 ';
 
 $username = user_getname();
