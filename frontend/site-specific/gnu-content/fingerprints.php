03afc580f91a1c8d6984eed26f5a5a6af359601e
Ineiev <ineiev@gnu.org>
2018-05-12 15:40:38 +0000

    Add SHA256 fingerprints

--- frontend/site-specific/gnu-content/fingerprints.php
+++ frontend/site-specific/gnu-content/fingerprints.php
@@ -2,9 +2,14 @@
 # vcs.savannah.gnu.org fingerprints; used in vcs-related pages.
 $vcs_fingerprints = '
 <pre>
-1024 80:5a:b0:0c:ec:93:66:29:49:7e:04:2b:fd:ba:2c:d5 (RSA)
-256 65:b8:1c:2f:82:7c:0e:39:e1:4a:63:f2:13:10:e8:9c (ECDSA)
-256 14:7b:c8:98:dd:06:08:97:8c:00:9d:d2:ae:85:c8:82 (ED25519)
+1024 MD5:80:5a:b0:0c:ec:93:66:29:49:7e:04:2b:fd:ba:2c:d5 (RSA)
+1024 SHA256:FYkx0iik+iBeCLRzvUyUSTRT98TEBBJoYuQsTXbyGL8 (RSA)
+
+256 MD5:65:b8:1c:2f:82:7c:0e:39:e1:4a:63:f2:13:10:e8:9c (ECDSA)
+256 SHA256:qRLLJ4w/GAeiDyYnbx4yWJbZXwGiYYxgNty7lAfUyuM (ECDSA)
+
+256 MD5:14:7b:c8:98:dd:06:08:97:8c:00:9d:d2:ae:85:c8:82 (ED25519)
+256 SHA256:o/oI4CKKcWc4cZvDFEdmOXsE3tiPP8bWa04h4bQjtV4 (ED25519)
 </pre>
 
 ';
