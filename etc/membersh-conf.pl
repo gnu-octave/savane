6df231dbef684f598daf876183b4d12aab81357d
Ineiev <ineiev@gnu.org>
2018-05-26 10:20:05 +0000

    add license and copyright notices

--- etc/membersh-conf.pl
+++ etc/membersh-conf.pl
@@ -1,10 +1,28 @@
 #!/usr/bin/perl
-# 
+#
 # Sample membersh conf (restricted shell)
+#
+# Copyright (C) 2006, 2017 Sylvain Beucler
+# Copyright (C) 2008       Aleix Conchillo Flaque <aleix@member.fsf.org>
+#
+# This file is part of Savane.
+#
+# Savane is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# Savane is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 # $use_cvs = "1";
 # $bin_cvs = "/usr/bin/cvs";
-# 
+#
 # $use_scp = "1";
 # $bin_scp = "/usr/bin/scp";
 # $regexp_scp = "^scp( -[prv])* -t (-- )?/(upload|var/ftp)";
