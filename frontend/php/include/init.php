6f41aab83a1be3f13205e1d2a739af6d6a3ce477
Ineiev <ineiev@gnu.org>
2019-01-09 13:34:47 +0000

    restore headers as of 589f47c6e60

--- frontend/php/include/init.php
+++ frontend/php/include/init.php
@@ -24,10 +24,9 @@
    not serve it by default.  */
 header('Content-Type: text/html; charset=utf-8');
 /* Disallow embedding in any frames.  */
-header('X-Frame-Options: SAMEORIGIN');
+header('X-Frame-Options: DENY');
 /* Declare more restrictions on how browsers may assemble pages.  */
 header("Content-Security-Policy: default-src 'self'; frame-ancestors 'none'; "
-       ."frame-src https://static.gnu.org/nosvn/banners/2018fundraiser/ "
        ."img-src 'self' static.fsf.org");
 # Database abstraction.
 require_once(dirname(__FILE__).'/database.php');
