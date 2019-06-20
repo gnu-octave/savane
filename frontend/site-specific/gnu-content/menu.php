fe5ca4598e8c726eb64c8a8a94a2c64e10ade150
Ineiev <ineiev@gnu.org>
2019-06-20 18:25:38 +0000

    use local copy of thin-image.png, i18nalize it

--- frontend/site-specific/gnu-content/menu.php
+++ frontend/site-specific/gnu-content/menu.php
@@ -7,7 +7,7 @@
 # Copyright (C) 2008, 2011, 2012, 2013 Karl Berry
 # Copyright (C) 2010, 2011, 2012 Michael J. Flickinger
 # Copyright (C) 2017 Bob Proulx
-# Copyright (C) 2017, 2018 Ineiev <ineiev@gnu.org>
+# Copyright (C) 2017, 2018, 2019 Ineiev <ineiev@gnu.org>
 #
 # This file is part of Savane.
 #
@@ -34,22 +34,21 @@ $HTML->menu_entry('//www.gnu.org/licenses/license-list.html',_('License List'));
 $HTML->menu_entry('//www.gnu.org/prep/ftp.html',_('GNU Mirrors'));
 $HTML->menuhtml_bottom();
 
-print '
-<li>
- <center>
-  <br />
-';
-  print '<a href="//www.fsf.org/associate/support_freedom/join_fsf?referrer=2442">'
-.'<img class="support-freedom"
-  src="//static.fsf.org/fsforg/img/thin-image.png" alt="'
-        ._('Support freedom').'" title="'
-        ._('Help protect your freedom, join the Free Software Foundation')
-        .'" /></a>';
-print '
- </center>
-</li>
-
-';
+print '<li>';
+print '<span class="button">';
+print utils_link ('//www.fsf.org/associate/support_freedom/join_fsf?referrer=2442',
+                  html_image (
+# TRANSLATORS: If Savane has a localized FSF logo in your language, fill its name
+# as the "translation" of this string; if it hasn't, repeat the original string
+# in your translation.
+                              _('thin-image.png'),
+                              array('alt' => _('Support freedom')),
+                              0) . "\n<br />"
+                  . _('Help us protect your freedom and the rights of computer
+users everywhere by becoming a member of the FSF.') . "\n<br /><strong>"
+                  . _('Join Now!') . '</strong>');
+print "</span>\n";
+print "</li>\n\n";
 
 $HTML->menuhtml_top(_('Free Software Foundation'));
 $HTML->menu_entry('//www.fsf.org/events/', _('Coming Events'));
@@ -73,7 +72,7 @@ $HTML->menu_entry('//puszcza.gnu.org.ua/',
                   _('Puszcza'));
 $HTML->menuhtml_bottom();
 
-// You can create other menus here, following the model above. They will be
-// shown in every page. You can also delete the above menu and leave
-// this page empty if you want to.
+# You can create other menus here, following the model above. They will be
+# shown in every page. You can also delete the above menu and leave
+# this page empty if you want to.
 ?>
