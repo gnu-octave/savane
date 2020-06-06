df3d3d1c61ce46f4f623e2b5b4eb10804201acb6
Ineiev <ineiev@gnu.org>
2020-06-06 18:32:26 +0000

    add copyright and license notices

--- frontend/site-specific/gnu-content/fingerprints.php
+++ frontend/site-specific/gnu-content/fingerprints.php
@@ -1,5 +1,22 @@
 <?php
 # vcs.savannah.gnu.org fingerprints; used in vcs-related pages.
+
+# Copyright (C) 2017, 2018, 2020 Ineiev <ineiev@gnu.org>
+#
+# This file is part of Savane.
+#
+# Savane is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# Savane is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 $vcs_fingerprints = '
 <pre>
 1024 MD5:80:5a:b0:0c:ec:93:66:29:49:7e:04:2b:fd:ba:2c:d5 (RSA)
