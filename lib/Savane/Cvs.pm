6c08b090de3fee5466b2878b86c4d08ba46f3481
Ineiev <ineiev@gnu.org>
2021-09-23 13:05:08 +0000

    refresh CVS hooks when running CvsMakeAreaSavannah

--- lib/Savane/Cvs.pm
+++ lib/Savane/Cvs.pm
@@ -2,7 +2,7 @@
 # Cvs-related subroutines.
 #
 # Copyright (C) 2003-2006 Mathieu Roy <yeupou--gnu.org>
-# Copyright (C) 2003-2006 Sylvain Beucler <beuc--beuc.net>
+# Copyright (C) 2003-2006, 2009 Sylvain Beucler <beuc--beuc.net>
 # Copyright (C) 2003-2006 Free Software Foundation, Inc.
 # Copyright (C) 2021 Ineiev
 #
@@ -34,6 +34,7 @@ our @ISA = qw(Exporter);
 our @EXPORT = qw(CvsMakeArea CvsMakeAreaAttic CvsMakeAreaSavannah WebCvsMakeAreaSavannahGNU WebCvsMakeAreaSavannahNonGNU );
 our $version = 1;
 
+our $dbd;
 
 # Make a cvs area.
 sub CvsMakeArea {
@@ -171,21 +172,226 @@ sub CvsMakeAreaAttic {
 }
 
 # Compute the type argument for new-savannah-project/new.py.
+# Historically, web type was (more) hardcoded, and its
+# range included 'translations'; as of 2021-09, it's defined
+# by the dir_type_homepage of the group_type table with the exception
+# of 'www'.
 sub CvsSavannahWebType {
     my ($name) = @_;
-
-    my $ws_type = 'non-gnu';
     my $dir_type = GetGroupTypeSettings(GetGroupType($name), 'dir_type_homepage');
-    # Historically, ws_type was (more) hardcoded, and its
-    # range included 'translations'; as of 2021-09, it's defined
-    # by the dir_type_homepage of the group_type table with the exception
-    # of 'www' (the exception actually doesn't matter very much because www
-    # repository already exists).
-    $ws_type = 'gnu' if $dir_type eq 'savannah-gnu';
-    if ($name eq 'www') {
-	$ws_type = 'www';
+
+    return 'www' if $name eq 'www';
+
+    # Return 'gnu' when $dir_type is 'savannah-gnu',
+    #        'non-gnu' when $dir_type is 'savannah-nongnu',
+    # else $dir_type (e.g. 'translations').
+    if ($dir_type =~ s/^savannah-//) {
+	$dir_type =~ s/^non/non-/;
     }
-    return $ws_type;
+    return $dir_type;
+}
+
+# Build the loginfo and commitinfo configuration for a single hook.
+sub BuildHooks {
+    my ($group, $repo, $match_type, $dir_list, $emails_notif,
+	$diff_on, $emails_diff, $branches, $hook_id) = @_;
+    my $warning = '';
+    my $log_accum = '';
+    $dir_list = '' if (!defined($dir_list));
+    $emails_diff = '' if (!defined($emails_diff));
+    $branches = '' if (!defined($branches));
+    
+    my @dirs = split(',', $dir_list);
+
+    # Check for spaces in dir names - this could be used to insert
+    # a custom command, such as rm -rf...
+    foreach my $dir (@dirs) {
+	# Use POSIX [:space:] instead of \s for completeness
+	# (check man perlre).
+	if ($dir =~ /[[:space:]]/) {
+	    return ('', '', "A space in the dir name '" . $dir . "'");
+	}
+    }
+
+    my $regexp;
+    if ($match_type eq 'ALL' || $match_type eq 'DEFAULT') {
+	$regexp = $match_type;
+    } elsif ($match_type eq 'dir_list') {
+	if (scalar(@dirs) == 0) {
+	    return  ('', '', $warning);
+	} elsif (scalar(@dirs) == 1) {
+	    $regexp = '^' . $dirs[0] . '\(/\|$\)';
+	} else {
+	    $regexp = '^\(' . join('\|', @dirs) . '\)\(/\|$\)';
+	}
+    } else {
+	$warning = "Unknown match type: $match_type";
+    }
+    $log_accum .= $regexp;
+    $log_accum .= ' /usr/local/bin/log_accum.pl ';
+
+    if ($emails_notif eq '') {
+	$warning = "No emails_notif - skipping...";
+	return  ('', '', $warning);
+    } else {
+	my @emails = split(',', $emails_notif);
+	$log_accum .= join('', map { "--mail-to $_ " } @emails);
+    }
+
+    if ($diff_on == 1) {
+	$log_accum .= '--send-diff';
+	$log_accum .= ' ';
+	if ($emails_diff ne '') {
+	    my @emails = split(',', $emails_diff);
+	    $log_accum .= join('', map { "--separate-diffs $_ " } @emails);
+	}
+    } else {
+	$log_accum .= '--no-send-diff';
+	$log_accum .= ' ';
+    }
+
+    if ($branches ne '') {
+	$log_accum .= join('', map { "--only-tag $_ " } split(',', $branches));
+    }
+
+    # Give a unique prefix for temporary files for each line.
+    # Some lines may run concurrently (eg. when regexp='ALL').
+    $log_accum .= '--file-prefix ' . $group . '_' . $hook_id;
+    $log_accum .= ' ';
+
+    my $config_file;
+    if ($repo eq 'sources') {
+	$config_file = 'log_accum-sources.config';
+    } else {
+	$config_file = 'log_accum-web.config';
+    }
+    $log_accum .= "--config /etc/$config_file";
+    $log_accum .= ' ';
+
+    $log_accum .= "%p %{sVv}";
+    $log_accum .= "\n";
+
+    my $commit_prep = "$regexp /usr/local/bin/commit_prep.pl ";
+    $commit_prep .= '-T ' . $group . '_' . $hook_id;
+    $commit_prep .= " %p\n";
+
+    return ($log_accum, $commit_prep, $warning);
+}
+
+sub BasicWebHook {
+    my ($name) = @_;
+    my $type = CvsSavannahWebType($name);
+    return "ALL echo 'Triggering webpages update...'; "
+                   . "cat > /dev/null; "
+                   . "curl http://www.gnu.org/new-savannah-project/new.py "
+                   . "-s -F type=$type -F project=`basename %r`\n"
+}
+
+# Update email notification hooks in commitinfo if needed.
+sub UpdateCvsNotifications {
+    my ($name, $dir_cvs, $repo) = @_;
+    my $warning = "";
+    my $loginfo = "";
+    my $commitinfo = "";
+
+    my $hooks_ref = $dbd->selectall_hashref("
+	SELECT groups.unix_group_name, cvs_hooks.*, cvs_hooks_log_accum.*
+	FROM groups JOIN cvs_hooks ON groups.group_id = cvs_hooks.group_id
+                    JOIN cvs_hooks_log_accum ON cvs_hooks.id = cvs_hooks_log_accum.hook_id
+        WHERE groups.unix_group_name = '" . $name . "'
+	      AND repo_name = '" . $repo . "'
+	ORDER by hook_id", 'hook_id');
+
+    foreach my $hook_id (keys %{$hooks_ref}) {
+	my ($log_accum, $commit_prep, $w)
+	    = BuildHooks($hooks_ref->{$hook_id}->{'unix_group_name'},
+			 $hooks_ref->{$hook_id}->{'repo_name'},
+			 $hooks_ref->{$hook_id}->{'match_type'},
+			 $hooks_ref->{$hook_id}->{'dir_list'},
+			 $hooks_ref->{$hook_id}->{'emails_notif'},
+			 $hooks_ref->{$hook_id}->{'enable_diff'},
+			 $hooks_ref->{$hook_id}->{'emails_diff'},
+			 $hooks_ref->{$hook_id}->{'branches'},
+			 $hook_id);
+	$loginfo .= $log_accum;
+	$commitinfo .= $commit_prep;
+	if ($w ne '') {
+	    $warning .= ' (' . $name . ':' . $repo . ': ' . $w . ')';
+	}
+    }
+
+    if ($repo eq 'web') {
+	$loginfo .= BasicWebHook($name);
+    }
+
+    sub update {
+        my $w = '';
+	my ($file, $replace_string) = @_;
+	my $text;
+	my $prev_text;
+	{
+	    local $/;
+	    undef $/; # slurp mode
+	    open(IN, "< $file") || ($w = " Cannot open $file: $! for reading.");
+	    $text = <IN>;
+	    close(IN);
+	}
+	$prev_text = $text;
+	$text =~ s,^#<savane>\n[[:space:]]*.*^#</savane>($),#<savane>\n$replace_string#</savane>,sm;
+	return $w if $prev_text eq $text;
+	# Find lines present in $to_replace, but absent in current file, and vice versa.
+	my $to_replace = $prev_text;
+	$to_replace =~ s,.*^#<savane>\n[[:space:]]*(.*)^#</savane>.*,$1,sm;
+        my @new_lines = split("\n", $replace_string);
+        my @old_lines = split("\n", $to_replace);
+	my %line_hash;
+	my $diff = '';
+	foreach my $val (@old_lines) {
+	    $line_hash{$val} = 0;
+	}
+	foreach my $val (@new_lines) {
+	    $line_hash{$val} = 0;
+	}
+
+	foreach my $val (@old_lines) {
+	    $line_hash{$val}--;
+	}
+	foreach my $val (@new_lines) {
+	    $line_hash{$val}++;
+	}
+
+	while (each %line_hash) {
+	    if ($line_hash{$_} > 0) {
+		$diff .= '+ ' . $_ . "\n";
+	    }
+	    if ($line_hash{$_} < 0) {
+		$diff .= '- ' . $_ . "\n";
+	    }
+	}
+	if ($diff ne '') {
+	    print "updating $file:\n$prev_text\n= vs =\n$text";
+	    print "=== difference:\n" . $diff;
+	} else {
+	    return $w;
+	}
+	return $w;
+
+	open(OUT, "> $file") || ($w .= " Cannot open $file: $! for writing.");
+	print OUT $text;
+	close(OUT);
+	return $w;
+    }
+    my $w;
+    $w = update("$dir_cvs/CVSROOT/commitinfo", $commitinfo);
+    if ($w ne '') {
+	$warning .= ' (' . $name . ':' . $repo . ':' . $w . ')';
+    }
+    $w = update("$dir_cvs/CVSROOT/loginfo", $loginfo);
+    if ($w ne '') {
+	$warning .= ' (' . $name . ':' . $repo . ':' . $w . ')';
+    }
+    return 0 if $warning eq '';
+    return ' ' . $dir_cvs . $warning;
 }
 
 # Make a cvs area at Savannah.
@@ -203,7 +409,7 @@ sub CvsMakeAreaSavannah {
 	$warning = " (The string \%PROJECT was not found, there may be a group type serious misconfiguration)";
     }
 
-    return 0 if (-e $dir_cvs);
+    return UpdateCvsNotifications($name, $dir_cvs, $repo_type) if (-e $dir_cvs);
 
     # Build the directory.
     my $mode = $is_public ? '2775' : '2770';
@@ -296,12 +502,9 @@ EOF
 #<savane>
 EOF
     if ($repo_type eq 'web') {
-        my $web_type = CvsSavannahWebType($name);
-        print FILE "ALL echo 'Triggering webpages update...'; "
-                   . "cat > /dev/null; "
-                   . "curl http://www.gnu.org/new-savannah-project/new.py "
-                   . "-s -F type=$web_type -F project=`basename %r`\n";
+        print FILE BasicWebHook($name);
     }
+
     print FILE <<EOF;
 #</savane>
 EOF
@@ -351,9 +554,11 @@ sub WebCvsMakeArea {
 	return 1;
     }
 
-    return 0 if (-e $dir_cvs);
-
+    # Give CvsMakeAreaSavannah a chance to update hooks
+    # before skipping the operations that are only needed for fresh repos.
+    my $have_dir_cvs = -e $dir_cvs;
     CvsMakeAreaSavannah($name,$dir_cvs,$is_public,'web');
+    return 0 if $have_dir_cvs;
 
     if ($update_acl) {
         # Allow modifications from group 'www' in the web module.
