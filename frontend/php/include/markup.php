62a08d4714e7a1ee2a8f3e3477ee996b6685f0ef
Ineiev <ineiev@gnu.org>
2021-11-09 11:37:04 +0000

    Preserve first space in line, Savannah sr #110562.

--- frontend/php/include/markup.php
+++ frontend/php/include/markup.php
@@ -240,6 +240,7 @@ function markup_preserve_spaces ($buf)
   $buf = preg_replace ('/  *(\n|$)/', '$1', $buf);
   $buf = preg_replace ('/ /', '&nbsp;', $buf);
   $buf = preg_replace ('/(([&]nbsp;)*)[&]nbsp;/', '$1 ', $buf);
+  $buf = preg_replace ('/(\n) /', '$1&nbsp;', $buf);
   $buf = preg_replace ('/^((<p>)?) /', '$1&nbsp;', $buf);
   return $buf;
 }
