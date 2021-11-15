80e6271e21e36b764ef3d522ddf74b007185fb32
Ineiev <ineiev@gnu.org>
2021-11-15 08:16:46 +0000

    hide emails in "Replaced by" for anonymous visitors

--- frontend/php/include/trackers/show.php
+++ frontend/php/include/trackers/show.php
@@ -547,10 +547,7 @@ function show_item_history ($item_id,$group_id, $no_limit=false)
               print utils_format_date($new_value_id, 'natural');
             }
           else
-            {
-              # It's a text zone then display directly.
-              print htmlspecialchars ($new_value_id);
-            }
+            print markup_basic ($value_id);
           print "</td>\n";
           print "</tr>\n";
         }
