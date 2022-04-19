7421d075f79dce36f8aaa7e42044e84e00401d37
Ineiev <ineiev@gnu.org>
2022-04-19 12:02:28 +0000

    add more detailed licensing terms

--- frontend/php/include/pwqcheck.php
+++ frontend/php/include/pwqcheck.php
@@ -1,8 +1,23 @@
 <?php
 # Check password strength.
 #
-# Copyright (C) 2000-2002,2010,2013,2016 Solar Designer
-# Copyright (C) 2017 Ineiev
+# Copyright (C) 2000-2002, 2010, 2013, 2016 Solar Designer
+# Copyright (C) 2017, 2022 Ineiev
+#
+# This file is part of Savane.
+#
+# Savane is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# Savane is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 # From:
 # http://www.openwall.com/articles/PHP-Users-Passwords#enforcing-password-policy
@@ -18,6 +33,22 @@
 # The original pwqcheck is not internationalized. Strings to localize
 # are taken from passwdqc_check.c (the 1.3.1 release); they are copyrighted
 # by Solar Designer.
+# The license for passwdqc_check.c is:
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
 
 $pwqcheck_messages_for_i18n = [
   _("Bad passphrase (is the same as the old one)"),
